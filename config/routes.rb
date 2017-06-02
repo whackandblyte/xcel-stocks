40f367a6be27a652fcff95829dcf2c76008e621d
Henrik Wollersheim <henrik@wollersheim.org>
2017-06-02 14:29:45 +0200

    Stocks can now be added to a users profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'users/my_portfolio'
 
   devise_for :users
+  resources :user_stocks, except: [:show, :edit, :update]
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
